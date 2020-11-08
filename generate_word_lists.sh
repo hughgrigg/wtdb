6614053cbbc0086d5470ee7e9f2d6a68fdb9ed94
Hugh Grigg <mhgrigg@fastmail.com>
2020-11-08 12:06:27 +0000

    Tweak word list generator script

--- generate_word_lists.sh
+++ generate_word_lists.sh
@@ -13,16 +13,12 @@ if [ ! -f /tmp/obscene.list ]; then
   wget -O /tmp/obscene.list 'https://github.com/LDNOOBW/List-of-Dirty-Naughty-Obscene-and-Otherwise-Bad-Words/raw/master/en'
 fi
 
-if [ ! -f /tmp/dictionaries.list ]; then
-  touch /tmp/dictionaries.list
-
-  if [ -f /usr/share/dict/british-english ]; then
-    cat /usr/share/dict/british-english | awk '$0 ~ /^[a-z]{2,10}$/' > /tmp/dictionaries.list
-  fi
-
-  if [ -f /usr/share/dict/american-english ]; then
-    cat /usr/share/dict/american-english | awk '$0 ~ /^[a-z]{2,10}$/' >> /tmp/dictionaries.list
-  fi
+echo > /tmp/dictionaries.list
+if [ -f /usr/share/dict/british-english ]; then
+  cat /usr/share/dict/british-english | awk '$0 ~ /^[a-z]{4,10}$/' > /tmp/dictionaries.list
+fi
+if [ -f /usr/share/dict/american-english ]; then
+  cat /usr/share/dict/american-english | awk '$0 ~ /^[a-z]{4,10}$/' >> /tmp/dictionaries.list
 fi
 
 cat /tmp/google-10000-english.txt /tmp/obscene.list \
@@ -30,7 +26,8 @@ cat /tmp/google-10000-english.txt /tmp/obscene.list \
     | sed 's/"//g' | sed "s/'//g" | sed -e 's/^\s+//g' -e 's/\s+$//g' \
     | sed 's/[“”]//g' | sed 's/_/ /g' \
     | awk '{print tolower($0)}' | awk 'length($0) > 1' \
-    | grep -v -P '^(un|re)' \
+    | grep -v -P '^(un|re|in)' \
+    | grep -v -P '(ing|er|ers|ness|ed)$' \
     | sort -u \
     | shuf > words.short.list
 
@@ -39,6 +36,7 @@ cat /tmp/google-10000-english.txt /tmp/obscene.list /tmp/dictionaries.list \
     | sed 's/"//g' | sed "s/'//g" | sed -e 's/^\s+//g' -e 's/\s+$//g' \
     | sed 's/[“”]//g' | sed 's/_/ /g' \
     | awk '{print tolower($0)}' | awk 'length($0) > 1' \
-    | grep -v -P '^(un|re)' \
+    | grep -v -P '^(un|re|in)' \
+    | grep -v -P '(ing|er|ers|ness|ed)$' \
     | sort -u \
     | shuf > words.long.list
